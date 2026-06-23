c03260a34c2be58fdfaefea5303bafc0ab2295c1
Danny <daniel.mijatovic09@gmail.com>
2026-06-23 17:30:56 +0200

    Update svelte.config.js

--- whisp_rebrand/svelte.config.js
+++ whisp_rebrand/svelte.config.js
@@ -1,5 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const config = {
+
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter()
+	}
+
+};
+
+export default config;import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
