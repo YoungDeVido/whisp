5740b304a77d78eb67b912edec8497bd02822aac
Danny <daniel.mijatovic09@gmail.com>
2026-06-23 18:19:24 +0200

    Update tailwind.config.js

--- whisp_rebrand/tailwind.config.js
+++ whisp_rebrand/tailwind.config.js
@@ -9,8 +9,8 @@ export default {
         mono: ["GeistMono"],
       },
       colors: {
-        orchid: "#673CE3",
-        "orchid-100": "#774BF3",
+        orchid: "#FF6B00",
+        "orchid-100": "#E65F00",
         primary: "#9C95AC",
         grey: "#30303A",
         "grey-100": "#373743",
